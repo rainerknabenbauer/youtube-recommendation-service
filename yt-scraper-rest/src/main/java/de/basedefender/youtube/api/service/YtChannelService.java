0ba7fd9a21b1bbc2b777fd3ed6acfe777d109094
nykon <rainer.knabenbauer@posteo.de>
2018-07-12 22:12:28 +0200

    Still fixing Lombok @RequiredArgsConstructor

--- yt-scraper-rest/src/main/java/de/basedefender/youtube/api/service/YtChannelService.java
+++ yt-scraper-rest/src/main/java/de/basedefender/youtube/api/service/YtChannelService.java
@@ -1,8 +1,17 @@
 package de.basedefender.youtube.api.service;
 
+import de.basedefender.youtube.api.util.YtChannelUtils;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.beans.ConstructorProperties;
+
 @Service
+@RequiredArgsConstructor
 public class YtChannelService {
+
+   // private final YtChannelUtils ytChannelUtils;
+
+
 
 }
--- yt-scraper-rest/src/test/java/de/basedefender/youtube/YtChannelUtilsTest.java
+++ yt-scraper-rest/src/test/java/de/basedefender/youtube/YtChannelUtilsTest.java
@@ -2,40 +2,30 @@ package de.basedefender.youtube;
 
 import com.google.api.services.youtube.model.SearchListResponse;
 import de.basedefender.youtube.api.util.YtChannelUtils;
-import de.basedefender.youtube.domain.EnvironmentVariablesNotSetException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-//@RunWith(SpringRunner.class)
 public class YtChannelUtilsTest {
 
     private YtChannelUtils ytChannelUtils;
     private static String gotoChannelId = "UCs_tLP3AiwYKwdUHpltJPuA";
-    private String apiKey;
 
     @Before
     public void init() {
         this.ytChannelUtils = new YtChannelUtils(10L);
-        setEnvironment();
     }
 
     @Test
-    public void searchVideosTest() {
+    public void searchVideosByChannelTest() {
+
         SearchListResponse response = ytChannelUtils.searchVideos(gotoChannelId);
+
         Assert.assertTrue(response.getItems().iterator().hasNext());
         Assert.assertEquals(10, response.getItems().size());
     }
 
-    /**
-     * Initializes required environment variables.
-     */
-    private void setEnvironment() {
-        try {
-            this.apiKey = System.getenv("youtube.api.key");
-        } catch (Exception ex) {
-            throw new EnvironmentVariablesNotSetException("Environment variable 'youtube.api.key' not set.", ex);
-        }
-    }
+    @Test
+    public void
 
 }
