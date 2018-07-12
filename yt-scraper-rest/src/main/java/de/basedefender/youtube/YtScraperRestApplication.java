043e4a44316cc4f15286f5c5629391998b01203a
nykon <rainer.knabenbauer@posteo.de>
2018-07-12 20:00:52 +0200

    Basic REST implementation done

--- yt-scraper-rest/src/main/java/de/basedefender/youtube/YtScraperRestApplication.java
+++ yt-scraper-rest/src/main/java/de/basedefender/youtube/YtScraperRestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
+@ComponentScan("de.basedefender.youtube")
 public class YtScraperRestApplication {
 
     public static void main(String[] args) {
